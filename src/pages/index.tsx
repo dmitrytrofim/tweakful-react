10524de589f205e3c376fe091cc9f90f985f7258
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2023-12-07 15:00:52 +0300

    +

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,38 @@ function Index() {
    <Main>
     <section>
      <Container>
-      <span></span>
+      <div className="">
+       <div className="">
+        <h1 className="">
+         This headline is the gist of the Your whole Website{' '}
+        </h1>
+        <p className="">
+         And the description of hero section is follow up message after the
+         punch of a great headline. So, make it short, simple, descriptive and
+         to the point.
+        </p>
+        <div className="">
+         <a href="#" className=""></a>
+         <p className="Here you can put secondary action statement & CTA"></p>
+        </div>
+        <div className="">
+         <img
+          className=""
+          src="./assets/img/svg/philips.svg"
+          alt="Test"
+          loading="lazy"
+         />
+        </div>
+       </div>
+       <div className="i-wrap">
+        <img
+         className="i-full"
+         src="./assets/img/woman-1.webp "
+         alt="Woman"
+         loading="lazy"
+        />
+       </div>
+      </div>
      </Container>
     </section>
    </Main>
